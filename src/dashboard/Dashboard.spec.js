2b55e73bc435c01de2d1113860621e2656c683da
jeff15113 <jeff15113@users.noreply.github.com>
2019-03-07 02:52:15 -0500

    Added basic tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
 // Test away
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  cleanup,
+  render,
+  fireEvent,
+  wait,
+  waitForElement
+} from "react-testing-library";
+
+import Dashboard from "./Dashboard";
+
+afterEach(cleanup);
+
+describe("Dashboard Component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Dashboard />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  cleanup,
+  render,
+  fireEvent,
+  wait,
+  waitForElement
+} from "react-testing-library";
+
+import Display from "./Display";
+
+afterEach(cleanup);
+
+describe("Display Component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Display />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("Default state is correct", () => {
+    const component = render(<Display />);
+    component.getByText("Unlocked");
+    component.getByText("Open");
+  });
+});
